0c807f190f99cf67b566e4ce1e18df2b357c2085
CintiaMaria7 <cintia@DESKTOP-NVGSP8I.localdomain>
2022-11-10 22:31:41 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 //Console log de teste
 console.log("Mais um commit para teste")
+console.log("Enviando para o GitHub")
